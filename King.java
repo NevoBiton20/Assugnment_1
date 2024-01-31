143035a776438a9995fb25f10ef9858f758b4e3e
shakedS10 <155729243+shakedS10@users.noreply.github.com>
2024-01-31 20:06:48 +0200

    Update King.java

--- King.java
+++ King.java
@@ -1,7 +1,7 @@
 public class King extends ConcretePiece {
     public King(Player player)
     {
-        this.places = new Stack<Position>();
+        
         this.player=player;
         this.type="♔";
     }
